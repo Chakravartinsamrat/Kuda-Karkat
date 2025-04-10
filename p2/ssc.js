96b2634a1b7543134cbda2c8c65f9142218a9d49
Piyush Chakarborthy <102757301+Chakravartinsamrat@users.noreply.github.com>
2025-04-10 15:50:47 +0530

    Commit

---
+++ p2/ssc.js
@@ -0,0 +1,22 @@
+var Size= 5;
+var growing =true;
+var ids = document.getElementById("demo");
+
+function animateText(){
+    if(growing){
+        Size+=5;
+        ids.innerHTML="Enlarging Deck";
+        ids.setAttribute("style", "font-size:"+Size+"px; color:red");
+        if(Size>=50){
+            growing=false;
+        }
+    }else{
+        Size-=5;
+        ids.innerHTML = "SHRINKING Deck";
+        ids.setAttribute("style", "font-size:" + Size + "px; color:blue");
+        if(Size<=5){
+            growing=true;
+        }
+    }
+}
+var intervalId = setInterval(animateText, 500); 
